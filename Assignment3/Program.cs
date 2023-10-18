5b44879cf0077121b39166d27f588989ab120bf6
Chirukaru36 <kpoojashree470@gmail.com>
2023-10-18 20:13:07 +0530

    Add project files.

---
+++ Assignment3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreetLibrary;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetingProgram greetingProgram = new GreetingProgram();
+            greetingProgram.OnGreet += (name) =>
+            {
+                Console.WriteLine($"Hello , {name} Nice to meet you");
+            };
+             Console.WriteLine("Welcome to the greeting program");
+
+            greetingProgram.GetUserInput();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ GreetLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetLibrary
+{
+
+    public class GreetingProgram
+    {
+      
+        public delegate void GreetingHandler(string name);
+
+  
+        public event GreetingHandler OnGreet;
+
+        public void GetUserInput()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+           
+            if (OnGreet != null)
+            {
+                OnGreet(name);
+            }
+        }
+    }
+}
